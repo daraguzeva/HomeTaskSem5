c37a3154c74fac5152fff53f3ccbc502f27b085e
Daria <lara-lorn@yandex.ru>
2023-01-29 14:53:05 +0400

    save task 038

--- Task038/Program.cs
+++ Task038/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+double max = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+double min = max; 
+for (int k = 0; k < array.Length; k++)
+{
+    if (array[k] < min)
+    {
+        min = array[k];
+    }
+}
+Console.WriteLine($"Максимальный элемент массива: {max}");
+Console.WriteLine($"Минимальный элемент массива: {min}");
+
+double deff = max - min; 
+
+Console.WriteLine($"Разница между max и min элементами: {deff}");
